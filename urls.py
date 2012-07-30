13c3399380cc59c3ccd9bd6e8337a5f85f55dcb8
VladoBrigant <vladimirbrigant@hotmail.com>
2012-07-30 13:03:57 -0400

    created route to admin

--- urls.py
+++ urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url, handler404, handler500
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
+from django.conf import settings
 
 from ella_hub.api import EllaHubApi
+
 
 admin.autodiscover()
 
@@ -17,6 +19,11 @@ admin_api.register_resources(resource_modules)
 urlpatterns = patterns('',
     # enable admin
     url(r'^admin/', include(admin.site.urls)),
+
+    (r'^admin-hope/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.PROJECT_ROOT + '/ella_hope/admin/',
+        'show_indexes': True,
+    }),
 
     (r'^', include(admin_api.urls)),
     #(r'^', include('ella.core.urls')),
