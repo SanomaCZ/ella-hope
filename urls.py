4a3254777589b266001bec7b9e2684f3a38e6c97
Mišo Belica <miso.belica@gmail.com>
2012-08-07 10:10:54 +0200

    Serve media files properly.

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ import os.path
 
 from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url, handler404, handler500
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
@@ -29,4 +30,4 @@ urlpatterns = patterns('',
 
     url(r'^', include(admin_api.urls)),
     url(r'^', include('ella.core.urls')),
-) + staticfiles_urlpatterns()
+) + staticfiles_urlpatterns() + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
