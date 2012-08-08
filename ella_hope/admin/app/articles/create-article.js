0e7de2529fa450144f1e0e7aca10619ee7151cf1
Jan Liska <mr.pohoda@gmail.com>
2012-08-08 17:02:07 +0200

    all crud operations working, create and update only with basic fields #1

--- ella_hope/admin/app/articles/create-article.js
+++ ella_hope/admin/app/articles/create-article.js
@@ -5,6 +5,9 @@ Create = can.Control({
 	 * @return {[type]}         [description]
 	 */
 	show: function(article){
+
+		console.log(article);
+
 		this.article = article;
 		if (!this.article) {
 			// we want to create a new article
@@ -28,18 +31,43 @@ Create = can.Control({
 	},
 
 	createArticle: function() {
-		console.log('create');
+
 		var form = this.element.find('form');
-			values = can.deparam(form.serialize());
-			
+		var values = form.serialize();
+		var values = can.deparam(values);
+		
+		delete values['authors'];
+		delete values['category'];
+		delete values['publish_to'];
+
+
+		values['announced'] = false;
+		values['app_data'] =  "{}";
+		values['authors'] =  [{"description": "", "email": "", "id": 1, "name": "Seocity", "resource_uri": "/admin-api/author/1/", "slug": "seocity", "text": ""}];
+		values['category'] =  "/admin-api/category/2/"; //Constructor;
+		values['last_updated'] =  "2012-08-07T09:47:44";
+		values['listings'] =  [];//Constructor[0];
+		values['photo'] =  null;
+		values['publish_from'] =  "2012-08-07T09:47:44";
+		values['publish_to'] =  null;
+		values['published'] =  true;
+		values['resource_uri'] =  "/admin-api/article/6/";
+		values['slug'] =  "dfgja";
+		values['static'] =  true;
+		values['url'] =  "http://example.com/sub/6-dfgj/";
+
+		if (!values['id']) delete values['id'];
+
 		console.log(values);
 		// validation
 		if(1) {
 			//this.article.attr(values).save();
-			delete values['id'];
+			//delete values['id'];
 			var a = new Article();
-			a.attr(values).save();
-			console.log(a.attr());
+			//a.attr(values).save();
+			a.attr(values);
+			//console.log(a);
+			a.save();
 			//this.hide();
 			can.route.attr({page:'articles'}, true);
 		}
--- ella_hope/admin/app/models/user.js
+++ ella_hope/admin/app/models/user.js
@@ -3,6 +3,13 @@ steal(
 	function($) {
 		User = can.Model({
 
+			// login
+			// curl --dump-header - -H "Content-Type: application/json" -X POST --data 'username=mrpohoda&password=mrpohoda' http://crawler.bfhost.cz:12345/admin-api/login/
+
+			// logout
+			// curl --dump-header - -H "Content-Type: application/json" -H "Authorization: ApiKey mrpohoda:5fe3435159771c589839f07e4446fd32df4a619d" -X POST http://crawler.bfhost.cz:12345/admin-api/logout/
+
+
   			findAll: 'POST http://crawler.bfhost.cz:12345/admin-api/login/',
   			
 		}, {});
