e0caebf763c11708adceb356923e00fb5ef98554
Mišo Belica <miso.belica@gmail.com>
2012-10-26 13:33:46 +0200

    Added new default API URL (personal development servers).

--- ella_hope/static/app/login/login.js
+++ ella_hope/static/app/login/login.js
@@ -20,8 +20,16 @@ steal(
 					'url':'http://crawler.bfhost.cz:12345/admin-api'
 				},
 				{
+					'name': "DEV Crawler",
+					'url':'http://crawler.bfhost.cz:3456/admin-api'
+				},
+				{
+					'name': "Localhost",
+					'url':'http://localhost:8000/admin-api'
+				},
+				{
 					'name': "DaZ",
-					'url':'http://google.com'
+					'url':'http://www.dumazahrada.cz/admin-api'
 				}
 			]
 		}
