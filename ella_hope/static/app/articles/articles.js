57bb1b48ab4c42c91e65d131a947090bc8aac81c
Jan Liska <mr.pohoda@gmail.com>
2012-10-29 14:39:21 +0100

    articles filtering finished

--- ella_hope/static/app/articles/articles.js
+++ ella_hope/static/app/articles/articles.js
@@ -23,7 +23,12 @@ steal(
 	{
 		defaults: {
 			initView : "//app/articles/views/init.ejs",
-			articleStates: ["added", "ready", "approved", "published", "postponed",	"deleted"]
+			articleStates: ["added", "ready", "approved", "published", "postponed",	"deleted"],
+			dateOptions: {	// https://github.com/eternicode/bootstrap-datepicker
+				format: 'yyyy-mm-dd',
+				weekStart: 1,
+				autoclose: true
+			}
 		}
 	},
 	/* @prototype */
@@ -59,21 +64,16 @@ steal(
 			}).then(function(){
 				// enable datepicker for publishFrom and publishTo
 				// https://github.com/eternicode/bootstrap-datepicker
-				var dateOptions = {
-					format: 'yyyy-mm-dd',
-					weekStart: 1,
-					autoclose: true
-				};
-				$("input[name=publish_from]").datepicker(dateOptions)
+				$("input[name=publish_from]").datepicker(self.options.dateOptions)
 					.on('changeDate', function(ev){
 						self.filterArticles();
 					});
-				$("input[name=publish_to]").datepicker(dateOptions)
+				$("input[name=publish_to]").datepicker(self.options.dateOptions)
 					.on('changeDate', function(ev){
 						self.filterArticles();
 					});
 
-				$("select[name=category]").on('change', function(ev){
+				$(".filter-form select").on('change', function(ev){
 						self.filterArticles();
 				});
 
@@ -110,7 +110,8 @@ steal(
 						self.articleCreate = new ArticleCreate(self.element, {
 							type: 'article',
 							article: article,
-							articleStates: self.options.articleStates
+							articleStates: self.options.articleStates,
+							dateOptions: self.options.dateOptions
 						});
 					});
 				}
@@ -121,7 +122,8 @@ steal(
 						self.articleCreate = new ArticleCreate(self.element, {
 							type: 'draft',
 							article: draft,
-							articleStates: self.options.articleStates
+							articleStates: self.options.articleStates,
+							dateOptions: self.options.dateOptions
 						});
 					});
 				}
@@ -189,7 +191,12 @@ steal(
 			// data from filter form
 			var data = {};
 
-			console.log('change');
+			//console.log('change');
+
+			// category
+			if ($("select[name=category]").val()) {
+				data.category__resource_uri = $("select[name=category]").val();
+			}
 
 			// publish_from
 			if ($("input[name=publish_from]").val()) {
@@ -201,10 +208,49 @@ steal(
 				data.publish_to__lte = $("input[name=publish_to]").val();
 			}
 
-			// category
-			if ($("select[name=category]").val()) {
-				data.category = $("select[name=category]").val();
+			// published
+			if ($("select[name=published]").val()) {
+				var published = $("select[name=published]").val();
+
+				var today = new Date();
+				today = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+				var yesterday = new Date();
+				yesterday.setDate(yesterday.getDate() - 1);
+				yesterday = yesterday.getFullYear()+'-'+(yesterday.getMonth()+1)+'-'+yesterday.getDate();
+
+				var weekBefore = new Date();
+				weekBefore.setDate(weekBefore.getDate() - 7);
+				weekBefore = weekBefore.getFullYear()+'-'+(weekBefore.getMonth()+1)+'-'+weekBefore.getDate();
+
+				var monthBefore = new Date();
+				monthBefore.setDate(monthBefore.getDate() - 31);
+				monthBefore = monthBefore.getFullYear()+'-'+(monthBefore.getMonth()+1)+'-'+monthBefore.getDate();
+
+				switch (published) {
+					case 'today' : data.publish_from__exact = today; break;
+					case 'yesterday' : data.publish_from__exact = yesterday; break;
+					case 'week' : {
+						data.publish_from__gte = weekBefore;
+						data.publish_from__lte = today;
+					} break;
+					case 'month' : {
+						data.publish_from__gte = monthBefore;
+						data.publish_from__lte = today;
+					} break;
+				}
 			}
+
+			// state
+			if ($("select[name=state]").val()) {
+				data.state = $("select[name=state]").val();
+			}
+
+			// author
+			if ($("select[name=author]").val()) {
+				data.authors__resource_uri = $("select[name=author]").val();
+			}
+
 
 			// show articles based on filter data
 			this.listArticles(data);
@@ -216,7 +262,7 @@ steal(
 		 * @param  {[type]} ev [description]
 		 * @return {[type]}    [description]
 		 */
-		'.filter-artucles click' : function(el, ev) {
+		'.filter-articles click' : function(el, ev) {
 
 			ev.preventDefault();
 
